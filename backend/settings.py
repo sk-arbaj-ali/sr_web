c43494cb1aa3e3af933587d28f55fdb70dd07271
SK ARBAJ ALI <skarbajali.one@outlook.com>
2024-08-14 19:05:10 +0530

    Fix : rolling back to the initial stage.

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'app.apps.AppConfig',
 ]
 
@@ -117,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
